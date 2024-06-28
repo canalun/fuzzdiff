c61265e3e4e8928c78bcd2d574fa01f05c27b2a7
canalun <i.am.kanaru.sato@gmail.com>
2024-06-28 15:06:21 +0900

    add some materials

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world!");
+import * as child_process from "child_process";
+
+const testNumber = 2;
+
+export function generatePages() {
+  child_process.exec(
+    `python3 ../domato/generator.py --output_dir ../fuzz --no_of_files ${testNumber}`
+  );
+}
+
+// WIP
