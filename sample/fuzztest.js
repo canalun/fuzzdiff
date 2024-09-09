65b7d53c7e0977b8c42bf61c45a16554eafc5d64
canalun <i.am.kanaru.sato@gmail.com>
2024-09-09 11:49:02 +0900

    update sample

--- sample/fuzztest.js
+++ sample/fuzztest.js
@@ -6,10 +6,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const options = {
-  dataNum: 3,
+  dataNum: 10,
   scriptFilePath: path.resolve(__dirname, "./fuzztest-sample.js"),
   outputPath: path.resolve(__dirname, "../fuzz"),
   performanceThreshold: 0.1,
+  isParallelEnabled: false,
 };
 
 fuzz(options);
