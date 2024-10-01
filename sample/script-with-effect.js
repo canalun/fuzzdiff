5d7bc08e783c84ea666176c58f85821cf07ae891
canalun <i.am.kanaru.sato@gmail.com>
2024-10-01 13:51:49 +0900

    add more prototype override

--- sample/script-with-effect.js
+++ sample/script-with-effect.js
@@ -1,9 +1,25 @@
 (function () {
-  const original = Element.prototype.setAttribute;
+  const originalSetAttribute = Element.prototype.setAttribute;
   Element.prototype.setAttribute = function (name, value) {
     if (name.includes("a")) {
-      console.log("setAttribute", name, value);
+      const arr = [];
+      for (let i = 0; i < 2; i++) {
+        arr.push(i);
+      }
     }
-    return original.apply(this, arguments);
+    return originalSetAttribute.apply(this, arguments);
+  };
+
+  const originalAppendChild = Element.prototype.appendChild;
+  Element.prototype.appendChild = function (child) {
+    if (child.tagName === "DIV") {
+      "aaa".includes("a");
+    }
+    return originalAppendChild.apply(this, arguments);
+  };
+
+  const originalGet = Map.prototype.get;
+  Map.prototype.get = function (key) {
+    return originalGet.apply(this, arguments);
   };
 })();
